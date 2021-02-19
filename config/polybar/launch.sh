6d034459a3886c5f671c9f6998739167fd477925
DimaGashko <dimagashko@gmail.com>
2021-02-19 08:13:10 +0200

    add DP1 polybar instanse

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -2,8 +2,9 @@
 
 killall -q polybar
 
-echo "---" | tee -a /tmp/polybar-primary.log /tmp/polybar-secondary.log
-polybar primary &
-polybar secondary &
+echo "---" | tee -a /tmp/polybar-eDP1.log /tmp/polybar-HDMI1.log /tmp/polybar-DP1
+polybar eDP1 &
+polybar HDMI1 &
+polybar DP1 &
 
 echo "Bars launched..."
