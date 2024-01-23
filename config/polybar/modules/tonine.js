872de9dce0adbbcdbf204d8f0b11954248c653b7
DimaGashko <dimagashko@gmail.com>
2024-01-23 06:19:53 +0200

    tonine

---
+++ config/polybar/modules/tonine.js
@@ -0,0 +1,14 @@
+
+const DAY = 1000 * 60 * 60 * 24;
+
+const today = new Date();
+const yearBeginning = new Date(today.getFullYear(), 0, 1);
+const september = new Date(today.getFullYear(), 8, 1);
+
+const daysToToday = (today - yearBeginning) / DAY;
+const daysToSeptember = (september - yearBeginning) / DAY;
+const restDays = daysToSeptember - daysToToday;
+
+const tonine = restDays / daysToSeptember * 100;
+
+console.log(`${restDays ^ 0}/${daysToSeptember ^ 0} (${daysToToday ^ 0}, ${tonine.toFixed(3)}%)`);
